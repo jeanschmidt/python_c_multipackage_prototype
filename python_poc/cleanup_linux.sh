5dcd1acaedec62fb658d96267effb9dd2de248c9
Jean Schmidt <contato@jschmidt.me>
2025-06-11 18:12:32 +0200

    20250611181232

--- python_poc/cleanup_linux.sh
+++ python_poc/cleanup_linux.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-rm -rf test_env
 rm -rf .venv
+rm -rf test_env
+
+pushd python_load_poc
 rm -rf *.egg-info
 rm -rf build
 pushd python_load_poc_c_extension
 make clean
+popd
 popd
--- python_poc/test_linux.sh
+++ python_poc/test_linux.sh
@@ -7,9 +7,12 @@ set -e
 # Do the build and install in a virtual environment
 virtualenv .venv
 source .venv/bin/activate
+
+pushd python_load_poc
 pip3 install -r requirements.txt
 python3 setup.py build
 python3 setup.py install
+popd
 
 # Do the testing (from another directory so it wont load the local python_load_poc)
 mkdir -p $TEST_FOLDER
