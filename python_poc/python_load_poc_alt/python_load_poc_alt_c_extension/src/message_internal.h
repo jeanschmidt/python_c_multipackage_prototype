7d1e34e27c702e8dc4351fc0ddaa3d7d4342ed86
Jean Schmidt <contato@jschmidt.me>
2025-06-16 18:18:30 +0200

    20250616181830

--- python_poc/python_load_poc_alt/python_load_poc_alt_c_extension/src/message_internal.h
+++ python_poc/python_load_poc_alt/python_load_poc_alt_c_extension/src/message_internal.h
@@ -1,6 +1,17 @@
 #ifndef MESSAGE_INTERNAL_H
 #define MESSAGE_INTERNAL_H
 
-void do_message_print(char *message);
+/* Define export/import macros for Windows DLL */
+#ifdef _WIN32
+    #ifdef BUILDING_DLL
+        #define EXPORT __declspec(dllexport)
+    #else
+        #define EXPORT __declspec(dllimport)
+    #endif
+#else
+    #define EXPORT
+#endif
+
+EXPORT void do_message_print(char *message);
 
 #endif
